2de91e6dfb606f60d4fee2fb0e135b36a5b91a30
rade <bajic.rade2@gmail.com>
2024-11-22 23:04:32 +0100

    perfect grid

--- src/patterns/11_grid_but_perfect/fragment.glsl
+++ src/patterns/11_grid_but_perfect/fragment.glsl
@@ -7,26 +7,41 @@ varying vec2 vUv;
 
 void main() {
 
+  // well my grid from previous example isn't centered
+  // which means rectangles touch sides
+
+
   // to create better looking grid I parametarized some things
-  float grid_items_by_row_or_column = 2.0;
-  
+  // I want to have this much rectangles
+  float grid_items = 10.0;  // this is number of grid items by row or column
+  // we can change this number above anytime we want more items by row/column
+
+
+  // center of the plane
+  // you will se why we use this  
   vec2 center = vec2(0.5, 0.5);
 
-  vec2 coords = vUv - center / grid_items_by_row_or_column / 2.0;
+  // I didnt came to this rationally
+  // I just try doing this kind of math on these values
+  // and it worked
+  vec2 newvUv = vUv - center / grid_items / 2.0;
+
+  // esencially because of this above,
+  // we will center our grid
 
   float strengthX = mod(
-    coords.x * grid_items_by_row_or_column,
+    newvUv.x * grid_items,
     1.0
   );
   float strengthY = mod(
-    coords.y * grid_items_by_row_or_column,
+    newvUv.y * grid_items,
     1.0
   );
 
   float strength = step(0.5, strengthX) + step(0.5, strengthY);
 
-
-  // strength = 1.0 - strength;
+  // to have white rectangles on black surface
+  strength = 1.0 - strength;
 
 
   gl_FragColor = vec4(vec3(strength), 1.0);
